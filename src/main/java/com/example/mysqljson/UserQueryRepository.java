c0d01247617d674eab5615de347c0e18f6a7b81a
Elizeu Santos <elizeusdsantos@gmail.com>
2019-02-16 08:38:52 -0200

    FIX indentation.

--- src/main/java/com/example/mysqljson/UserQueryRepository.java
+++ src/main/java/com/example/mysqljson/UserQueryRepository.java
@@ -1,6 +1,8 @@
-package com.example.mysqljson.domain;
+package com.example.mysqljson;
 
+import com.example.mysqljson.domain.UserQuery;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserQueryRepository extends CrudRepository<UserQuery, Long> {
+
 }
--- src/main/java/com/example/mysqljson/domain/UserQuery.java
+++ src/main/java/com/example/mysqljson/domain/UserQuery.java
@@ -1,94 +1,103 @@
 package com.example.mysqljson.domain;
 
+import com.example.mysqljson.StringMultimapConverter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import javax.persistence.*;
 import java.util.Objects;
 import java.util.StringJoiner;
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserQuery {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String userId;
-    private String name;
-    private Boolean active;
-    @Convert(converter = JsonUtil.class)
-    @Column(columnDefinition = "JSON")
-    private Query query;
 
-    public Long getId() {
-        return id;
-    }
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String userId;
+  private String name;
+  private Boolean active;
+  @Convert(converter = StringMultimapConverter.class)
+  @Column(columnDefinition = "JSON")
+  private Query query;
 
-    public UserQuery setId(Long id) {
-        this.id = id;
-        return this;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public String getUserId() {
-        return userId;
-    }
+  public UserQuery setId(Long id) {
+    this.id = id;
+    return this;
+  }
 
-    public UserQuery setUserId(String userId) {
-        this.userId = userId;
-        return this;
-    }
+  public String getUserId() {
+    return userId;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public UserQuery setUserId(String userId) {
+    this.userId = userId;
+    return this;
+  }
 
-    public UserQuery setName(String name) {
-        this.name = name;
-        return this;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public Boolean getActive() {
-        return active;
-    }
+  public UserQuery setName(String name) {
+    this.name = name;
+    return this;
+  }
 
-    public UserQuery setActive(Boolean active) {
-        this.active = active;
-        return this;
-    }
+  public Boolean getActive() {
+    return active;
+  }
 
-    public Query getQuery() {
-        return query;
-    }
+  public UserQuery setActive(Boolean active) {
+    this.active = active;
+    return this;
+  }
 
-    public UserQuery setQuery(Query query) {
-        this.query = query;
-        return this;
-    }
+  public Query getQuery() {
+    return query;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserQuery userQuery = (UserQuery) o;
-        return Objects.equals(id, userQuery.id) &&
-                Objects.equals(userId, userQuery.userId) &&
-                Objects.equals(name, userQuery.name) &&
-                Objects.equals(active, userQuery.active) &&
-                Objects.equals(query, userQuery.query);
-    }
+  public void setQuery(Query query) {
+    this.query = query;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, userId, name, active, query);
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    UserQuery userQuery = (UserQuery) o;
+    return Objects.equals(id, userQuery.id) &&
+        Objects.equals(userId, userQuery.userId) &&
+        Objects.equals(name, userQuery.name) &&
+        Objects.equals(active, userQuery.active) &&
+        Objects.equals(query, userQuery.query);
+  }
 
-    @Override
-    public String toString() {
-        return new StringJoiner(", ", UserQuery.class.getSimpleName() + "[", "]")
-                .add("id=" + id)
-                .add("userId='" + userId + "'")
-                .add("name='" + name + "'")
-                .add("active=" + active)
-                .add("query=" + query)
-                .toString();
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, userId, name, active, query);
+  }
+
+  @Override
+  public String toString() {
+    return new StringJoiner(", ", UserQuery.class.getSimpleName() + "[", "]")
+        .add("id=" + id)
+        .add("userId='" + userId + "'")
+        .add("name='" + name + "'")
+        .add("active=" + active)
+        .add("query=" + query)
+        .toString();
+  }
 }
